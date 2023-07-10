52efbfd29ae567cc056cedf1394b7de9814955c1
Chandru-Ghub <chandruinfo455@gmail.com>
2023-07-10 11:42:38 +0530

    image added

--- src/All.js
+++ src/All.js
@@ -3,25 +3,31 @@ import { useState } from 'react'
 import { NavLink } from "react-router-dom";
 import Software from './Software';
 import {Routes,Route} from 'react-router-dom';
+import img1 from './1.webp'
+import img2 from './skills.jpg'
+import img3 from './react-1536x804.jpg'
+import img4 from './backend-skills.jpg'
+import img5 from './software-engineering-internships.webp'
+import img6 from './Gray-Green-Illustration-Business-Blog-Banner-2048x1152.png'
 
 const All = () => {
     const [card,setCard]=useState(
         [
           {
             id : '1',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2023/06/software-engineering-internships.png',
+            image:'https://www.scnsoft.com/blog-pictures/testing/testing-trends-2019.png',
             title:'7 Powerful Steps to Land a Software Developer Internship',
             tag:'Software development is set to grow at the rate of 25% between 2021-2031. An illustrious'
           },
           {
             id : '2',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2023/03/backend-skills.jpg',
+            image:'http://www.guvi.in/blog/wp-content/uploads/2023/03/backend-skills.jpg',
             title:'Top 7 Back-End Developer Skills You Should Know',
             tag:'We all rely on the internet for everything. Open google and ask your doubt, and'
           },
           {
             id : '3',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2023/03/skills.jpg',
+            image:{img2},
             title:'Top 10 Front-End Developer Skills You Should Know',
             tag:'Websites nowadays are eye-catchy, and also responsive at the same time. Ever wondered who’s the'
           },
@@ -33,19 +39,18 @@ const All = () => {
           },
           {
             id : '5',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2022/11/Gray-Green-Illustration-Business-Blog-Banner-2048x1152.png',
-            title:'5 Tips to negotiate your salary as a Full Stack Developer',
+            image:{img6},
             tag:'Negotiation is such an important life skill. We are negotiating constantly, whether it’s for salary,'
           },
           {
             id : '6',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2022/06/1.-Amazon-fsd-salary-2048x1072.png',
+            image:{img1},
             title:'Amazon Full Stack Developer Salaries In India',
             tag:'Full Stack Developer salaries at Amazon range from ₹ 4.5 Lakhs to ₹ 46 Lakhs'
           },
           {
             id : '7',
-            image:'https://www.guvi.in/blog/wp-content/uploads/2021/05/react-1536x804.jpg',
+            image:{img3},
             title:'6 Essential Prerequisites For Learning ReactJS',
             tag:'Have you ever wished to create your own modern website or app with high performance'
           },
